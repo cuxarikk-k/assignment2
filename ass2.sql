3fd95903a49775ded93fae65b771974f6e8d7ade
Mariana Konovalenko <mkonovalenko1@kse.org.ua>
2025-10-22 19:45:46 +0300

    Update ass2.sql

--- ass2.sql
+++ ass2.sql
@@ -40,25 +40,26 @@ create table recipeUsage (
 SELECT 
     c.name, 
     AVG(c.popularity_score) AS global_popularity,
-    SUM(ru.volumeMl * i.cost_usd) AS total_recipe_cost 
+    SUM(ru.volumeMl * i.cost_usd) AS total_recipe_cost
 FROM 
     cocktails c
 JOIN 
     recipeUsage ru ON c.cocktail_id = ru.cocktail_id
 JOIN 
     ingredients i ON ru.ingredient_id = i.ingredient_id
+CROSS JOIN (
+    SELECT AVG(cost_usd) AS global_cost
+    FROM ingredients
+) AS avg_cost
 WHERE 
     c.category = 'classic'
     AND ru.usage_date >= '2024-10-01' 
-    -- blocks effectiveness
-    AND i.cost_usd > (
-        SELECT AVG(cost_usd) 
-        FROM ingredients
-    )
+    AND i.cost_usd > avg_cost.global_cost -- Використовуємо єдине обчислене значення
 GROUP BY 
     c.cocktail_id, c.name
 ORDER BY 
-    global_popularity DESC
+    global_popularity DESC,
+    total_recipe_cost desc
 LIMIT 10;
 
 -- indexes
@@ -81,17 +82,18 @@ select
 	/*+ JOIN_FIXED_ORDER(a, i, r, c) */
 	c.name, 
     avg(c.popularity_score) as global_popularity,
-    sum(r.volumeMl *i.cost_usd) as total_recipe_cost
-from cocktails c
-join recipeUsage r on c.cocktail_id = r.cocktail_id
+    sum(r.volumeMl * i.cost_usd) as total_recipe_cost
+from cocktails c use index (idx_cocktails_category_popularity)
+join recipeUsage r use index (idx_cocktails_usage_date)
+on c.cocktail_id = r.cocktail_id
 join ingredients i on r.ingredient_id = i.ingredient_id
 join averageCost a
 where
 	c.category = 'classic'
     and r.usage_date >= '2024-10-01' -- rewriting - change date_sub to constant date for index range scan
-    and i.cost_usd >= a.global_cost
+    and i.cost_usd > a.global_cost
 group by c.cocktail_id, c.name
-order by global_popularity desc
+order by global_popularity desc, total_recipe_cost desc
 limit 10;
 
 -- The core issue was slow query performance on millions of rows due to inefficient calculation and scanning. Our goal was to achieve identical results but at high speed.
@@ -102,5 +104,5 @@ limit 10;
 -- Targeted Indexing: We created three specific indexes to support every WHERE, JOIN, and ORDER BY condition.
 -- Bonus Hint: We added /*+ JOIN_FIXED_ORDER(...) */ to override the optimizer and guarantee the best possible join sequence, starting with the smallest, pre-calculated results.
 
--- Conclusion
+-- Conclusion:
 -- The EXPLAIN ANALYZE confirms our success: the optimized query shifts execution from slow Full Scans and filesort to high-speed Index Range Access.
